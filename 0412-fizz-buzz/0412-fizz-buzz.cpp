163b39d0244f05931c1988157bea6c913948e644
Animesh Singh <animeshsingh519@gmail.com>
2026-07-05 23:49:53 +0530

    Time: 1 ms (54.83%), Space: 11.8 MB (17.51%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+
+        for(int i=1;i<=n;i++){
+            float div3 = i/3.0;
+            float div5 = i/5.0;
+            int int3 = div3;
+            int int5 = div5;
+            float res3 = div3-int3;
+            float res5 = div5-int5;
+
+            if(res3==0 && res5==0) ans.push_back("FizzBuzz");
+            else if(res3==0 && res5!=0) ans.push_back("Fizz");
+            else if(res3!=0 && res5==0) ans.push_back("Buzz");
+            else ans.push_back(to_string(i));
+        }
+        return ans;
+    }
+};
